5944a36b9293b8c1f5edfefb5113875013251a77
julio-cesar-lacerda <60928218+julio-cesar-lacerda@users.noreply.github.com>
2020-02-19 18:18:29 -0300

    Create personal.h
    Criei este biblioteca  para estudantes que estudam a linguagem de programação em c e que gostam de personalizar o projeto em que estão trabalhando para torná-lo bonito e interessante. Aviso: este comando funciona apenas no sistema operacional linux.

---
+++ personal.h
@@ -0,0 +1,48 @@
+// +--------------------------------------------------+
+//            COMANDO PARA MUDAR  LETRA              
+// +--------------------------------------------------+
+#define NEGRITO printf("\33[1m"); //NEGRITO.        						   
+#define FOSCO printf("\33[2m"); //FOSCO.        						           						  
+#define ITAlICO printf("\33[3m"); //ITÁlICO.        						    
+#define SUB printf("\33[4m"); //SUBLINHADO.        					   
+#define PICA printf("\33[5m"); //PICA PICA.        					   
+#define COR printf("\33[7m"); //COR DE REALCE.        				   
+#define ESCONDE printf("\33[8m");//ESCONDE.        						   
+#define RASURA printf("\33[9m"); //RASURA.       						   
+#define SUBLINHADO printf("\33[21m"); //SUBLINHADO COM LINHA DUPLA.        	   
+#define LP printf("\33[30m"); //LETRA PRETA        					   
+#define LZC printf("\33[31m"); //LETRA AZUL_ClARO        				   
+#define LV printf("\33[32m"); //LETRA VERDE        					   
+#define LL printf("\33[33m"); //LETRA LARANJA        				   
+#define LA printf("\33[34m"); //LETRA AZUL        					   
+#define LR printf("\33[35m"); //LETRA ROSA        					   
+#define LAQ printf("\33[36m"); //LETRA AGUA        					   
+#define LB printf("\33[37m"); //LETRA BRANCO
+// +--------------------------------------------------+
+// +-------------------------------------------------+
+// 	  	COMANDO PARA MUDAR FUNDO TELA 		      
+// +-------------------------------------------------+
+#define FP printf("\33[40m"); //FUNDO_PRETO						      
+#define FV printf("\33[41m"); //FUNDO_AZUL_ClARO						  
+#define FLL printf("\33[42m"); //FUNDO_VERDE						      
+#define FAL printf("\33[43m"); //FUNDO_LARANJA						  
+#define FRS printf("\33[44m"); //FUNDO_AZUL						      
+#define FRR printf("\33[45m"); //FUNDO_ROSA						      
+#define FAA printf("\33[46m"); //FUNDO_AGUA						      
+#define FBB printf("\33[47m"); //FUNDO_BRANCO						      
+#define FC printf("\33[100m"); //FUNDO_CINZA						      
+#define FL printf("\33[101m"); //FUNDO_LARANJA						  
+#define FNV printf("\33[102m"); //FUNDO_VERDE						      
+#define FNAL printf("\33[103m"); //FUNDO_AMARELO						  
+#define FA printf("\33[104m"); //FUNDO_AZUL						      
+#define FR printf("\33[105m"); //FUNDO_ROSA						      
+#define FAC printf("\33[106m"); //FUNDO_AZUL_CLARO					  
+#define FB printf("\33[107m"); //FUNDO_BRANCO
+// +--------------------------------------------------+
+// +--------------------------------------------------+
+//            COMANDO PARA MUDAR  LETRA              
+// +--------------------------------------------------+
+#define DTC printf("\33[0m"); //DELETE TODO OS COMANDO \33[              
+#define DCL printf("\33[39m"); //DELETE COMANDO LETRA \33[               
+#define DCF printf("\33[49m"); //DELETE COMANDO DE FUNDO TELA \33[       
+// +--------------------------------------------------+
